81d20559b10dc8f3283aa2562b224efc65722d7b
Panteha <panteha.ahmadi@gmail.com>
2017-06-19 16:32:05 +0100

    Added test for showing notes inside notes array

--- tests/note-list-tests.js
+++ tests/note-list-tests.js
@@ -7,3 +7,17 @@ function testThatAddNoteCanAddANewNoteObject() {
 }
 
 testThatAddNoteCanAddANewNoteObject();
+
+
+
+function testThatReturnsAllTheNoteObjects(){
+  var noteList = new NoteList();
+  note1 = new Note("1");
+  note2 = new Note("2");
+  noteList.addNewNote(note1);
+  noteList.addNewNote(note2);
+  assert.isTrue(noteList.showNotes()[0] === note1);
+  assert.isTrue(noteList.showNotes()[1] === note2);
+}
+
+testThatReturnsAllTheNoteObjects();
