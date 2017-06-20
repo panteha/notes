7f65ed14578d3919526601f24bacbf6db63cb5d2
Panteha <panteha.ahmadi@gmail.com>
2017-06-20 15:39:39 +0100

    Unresolved

--- controller/note-controller.js
+++ controller/note-controller.js
@@ -1,20 +1,16 @@
-// var NoteList = require("./lib/note-list-model.js").NoteList;
 'use strict';
 
 (function(exports){
 
   var NoteController = function(notelist){
     this.notelist = notelist;
-    this.notelist.addNewNote("Favourite drink: seltzer");
-    // notelistview = new NoteListView(notelist)
   }
 
   NoteController.prototype.insertHtml = function(){
-    notelistview.generateHtml()
   }
 
-  NoteController.prototype.createView = function(notelist){
-    (new NoteListView(notelist)).noteList;
+  NoteController.prototype.createView = function(var notelistview = new NoteListView(notelist)){
+    notelistview.generateHtml();
   }
 
   exports.NoteController = NoteController;
