d345e52186193d48e29722b4e5a3d17e3c716e4f
Panteha <panteha.ahmadi@gmail.com>
2017-06-21 21:43:16 +0100

    Added onHashChange method

--- controller/note-controller.js
+++ controller/note-controller.js
@@ -5,13 +5,30 @@
   var NoteController = function(notelist){
     this.notelist = notelist;
     this.notelistview = new NoteListView(notelist);
-  }
+    this.noteId = null;
+    var note_controller = this;
+    window.onhashchange = function() {
+      note_controller.onHashChange();
+    };
+  };
 
   NoteController.prototype.createView = function(element){
-    element.innerHTML = this.notelistview.generateHtml();
-  }
+    if (this.noteId == null) {
+      element.innerHTML = this.notelistview.generateHtml();
+    } else {
+      var note = this.notelist.notes[this.noteId]
+      var noteView = new NoteView(note);
+      element.innerHTML = noteView.noteGenerateHtml();
+    }
+  };
+
+  NoteController.prototype.onHashChange = function() {
+    // console.log(location.hash);
+    // console.log(location.hash.substring(7,8));
+    this.noteId = location.hash.substring(7,8);
+    this.createView(document.getElementById(id='app'));
+  };
 
   exports.NoteController = NoteController;
 
 })(this);
-
